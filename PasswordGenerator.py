62d1dea22d1c2d1d2e4f0de48ed5803cc87a25c3
Bruno Borges Martim <martim.bruno@protonmail.com>
2023-02-18 12:18:18 -0300

    Include the numbers in the string

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -8,8 +8,9 @@ while True:
     lower_character = "abcdefghijklmnopqrstuvwxyz"
     upper_character = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     symbols = "!@#$%&*()_+=´^~{[}]:;.>,<"
+    numbers = "0123456789"
     
-    string = lower_character + upper_character + symbols
+    string = lower_character + upper_character + symbols + numbers
 
     # Get the random seed for choices in string
     response = requests.get('https://www.random.org/integers/?num=1&min=1&max=1000&col=1&base=10&format=plain&rnd=new') 
